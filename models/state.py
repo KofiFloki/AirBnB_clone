3b7ff70546f94e00286d791ad4ec6b0784b9f5a7
KofiFloki <Jeremiah.martey@yahoo.com>
2023-05-12 08:53:00 -0700

    project commit

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+"""The class State inheriting from the BaseModel Class"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """This Class State inherits from the Class BAseModel"""
+
+    name = ""
